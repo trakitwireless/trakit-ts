e6c77800266f7b7b54dc93453c1ab51397b1ccf0
Alex Lein <alein@trakitwireless.com>
2025-01-22 12:04:45 -0500

    Far less useless comments

--- objects/API/Constants.ts
+++ objects/API/Constants.ts
@@ -1,74 +1,51 @@
-﻿/** @type {Function} */
-export const SET_TIMER = setTimeout;
-/** @type {Function} */
-export const SET_EVERY = setInterval;
-/** @type {Function} */
-export const CLEAR_TIMER = clearTimeout;
-/** @type {Function} */
-export const CLEAR_EVERY = clearInterval;
-/** @type {Function} */
-export const INT = parseInt;
-/** @type {Function} */
-export const FLOAT = parseFloat;
+﻿/**
+ * Timer methods that get used many places.
+ */
+export const
+	SET_TIMER = setTimeout,
+	SET_EVERY = setInterval,
+	CLEAR_TIMER = clearTimeout,
+	CLEAR_EVERY = clearInterval;
 
-/** @type {Object} */
-export const MATH = Math;
-/** @type {number} */
-export const PI = MATH.PI;
-/** @type {number} */
-export const HALF_PI = PI / 2;
-/** @type {Function} */
-export const ABS = MATH.abs;
-/** @type {Function} */
-export const ACOS = MATH.acos;
-/** @type {Function} */
-export const ASIN = MATH.asin;
-/** @type {Function} */
-export const ATAN = MATH.atan;
-/** @type {Function} */
-export const ATAN2 = MATH.atan2;
-/** @type {Function} */
-export const CEIL = MATH.ceil;
-/** @type {Function} */
-export const COS = MATH.cos;
-/** @type {Function} */
-export const EXP = MATH.exp;
-/** @type {Function} */
-export const FLOOR = MATH.floor;
-/** @type {Function} */
-export const LOG = MATH.log;
-/** @type {Function} */
-export const MAX = MATH.max;
-/** @type {Function} */
-export const MIN = MATH.min;
-/** @type {Function} */
-export const POW = MATH.pow;
-/** @type {Function} */
-export const RANDOM = MATH.random;
-/** @type {Function} */
-export const ROUND = MATH.round;
-/** @type {Function} */
-export const SIN = MATH.sin;
-/** @type {Function} */
-export const SQRT = MATH.sqrt;
-/** @type {Function} */
-export const TAN = MATH.tan;
 /**
- * Does fast 32bit uint multiplication (from C++).  Used mostly in ASM.
- * @param a
- * @param b
- **/
-export const IMUL = MATH["imul"] || function imul(a: number, b: number): number {
-	const ah = (a >>> 16) & 0xffff;
-	const al = a & 0xffff;
-	const bh = (b >>> 16) & 0xffff;
-	const bl = b & 0xffff;
-	// the shift by 0 fixes the sign on the high part
-	// the final |0 converts the unsigned value into a signed value
-	return ((al * bl) + (((ah * bl + al * bh) << 16) >>> 0) | 0);
-};
+ * Parsing functions that get used all over.
+ */
+export const
+	INT = parseInt,
+	FLOAT = parseFloat;
 
-/** @type {number} */
-export const RADIANS_TO_DEGREES = 180 / PI;	// radians to degrees: value * (180 / pi)
-/** @type {number} */
-export const DEGREES_TO_RADIANS = PI / 180;	// degrees to radians: value * (pi / 180)
+/**
+ * Maths (as the Brits would say).
+ */
+export const
+	MATH = Math,
+	PI = MATH.PI,
+	ABS = MATH.abs,
+	ACOS = MATH.acos,
+	ASIN = MATH.asin,
+	ATAN = MATH.atan,
+	ATAN2 = MATH.atan2,
+	CEIL = MATH.ceil,
+	COS = MATH.cos,
+	EXP = MATH.exp,
+	FLOOR = MATH.floor,
+	LOG = MATH.log,
+	MAX = MATH.max,
+	MIN = MATH.min,
+	POW = MATH.pow,
+	RANDOM = MATH.random,
+	ROUND = MATH.round,
+	SIN = MATH.sin,
+	SQRT = MATH.sqrt,
+	TAN = MATH.tan;
+
+/**
+ * Constant values used for calculations in geometry and geography.
+ * half of pi (used in mercator projects)
+ * radians to degrees: value * (180 / pi)
+ * degrees to radians: value * (pi / 180)
+ */
+export const
+	HALF_PI = PI / 2,
+	RADIANS_TO_DEGREES = 180 / PI,
+	DEGREES_TO_RADIANS = PI / 180;
