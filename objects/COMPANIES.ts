e4ae85aa8f5e49f7fa32358a91320e877e5205f3
Alex Lein <alein@trakitwireless.com>
2025-02-07 15:41:41 -0500

    Moved main company store

---
+++ objects/COMPANIES.ts
@@ -0,0 +1,7 @@
+import { ulong } from './API/Types';
+import { Company } from './Companies/Company';
+
+/**
+ * 
+ */
+export const COMPANIES: Map<ulong, Company> = new Map;
--- objects/Companies/Companies.ts
+++
@@ -1,7 +0,0 @@
-import { ulong } from '../API/Types';
-import { Company } from './Company';
-
-/**
- * 
- */
-export const COMPANIES: Map<ulong, Company> = new Map;
