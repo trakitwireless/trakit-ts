aafc71cd44df6fa9c7a3154769fc902ce1da1ed2
Alex Lein <alein@trakitwireless.com>
2025-02-07 15:33:02 -0500

    Added ctors to policy classes

--- objects/Companies/PasswordPolicy.ts
+++ objects/Companies/PasswordPolicy.ts
@@ -1,4 +1,5 @@
-﻿import { byte } from "../API/Types";
+﻿import { ID } from "../API/Functions";
+import { byte } from "../API/Types";
 import { PasswordExpiryMode } from "./PasswordExpiryMode";
 
 /**
@@ -8,33 +9,39 @@ export class PasswordPolicy {
 	/**
 	 * The minimum number of characters required.
 	 */
-	minimumLength: byte = NaN;
+	minimumLength: byte;
 	/**
 	 * Do passwords require alphabetical characters.
 	 */
-	includeLetters: boolean = false;
+	includeLetters: boolean;
 	/**
 	 * Do passwords require numeric characters.
 	 */
-	includeNumbers: boolean = false;
+	includeNumbers: boolean;
 	/**
 	 * Do passwords require upper-case and lower-case letters.
 	 */
-	includeUpperLower: boolean = false;
+	includeUpperLower: boolean;
 	/**
 	 * Do passwords require non-alphanumeric characters.
 	 */
-	includeSpecial: boolean = false;
+	includeSpecial: boolean;
 	/**
 	 * Defines how passwords expire.
 	 */
-	expireMode: PasswordExpiryMode = PasswordExpiryMode.never;
+	expireMode: PasswordExpiryMode;
 	/**
 	 * The threshold for expiry.
 	 */
-	expireThreshold: byte = NaN;  // days
+	expireThreshold: byte;  // days
 	
 	constructor(json: any = null) {
-
+		this.minimumLength = ID(json["minimumLength"]);
+		this.includeLetters = !!json["includeLetters"];
+		this.includeNumbers = !!json["includeNumbers"];
+		this.includeUpperLower = !!json["includeUpperLower"];
+		this.includeSpecial = !!json["includeSpecial"];
+		this.expireMode = PasswordExpiryMode[json["expireMode"] as PasswordExpiryMode] ?? PasswordExpiryMode.never;
+		this.expireThreshold = ID(json["expireThreshold"]);
 	}
 }
--- objects/Companies/SessionPolicy.ts
+++ objects/Companies/SessionPolicy.ts
@@ -1,4 +1,4 @@
-﻿import { byte, ushort } from "../API/Types";
+﻿import { byte, ipv4, ushort } from "../API/Types";
 import { SessionMultiUser } from "./SessionMultiUser";
 
 /**
@@ -8,25 +8,35 @@ export class SessionPolicy {
 	/**
 	 * The list of applications users are allowed to use to create sessions.
 	 */
-	applications: string[] = [];
+	applications: string[];
+	/**
+	 * Ranges of IPv4 addresses (using Cisco notation) to limit session creation to certain IPs.
+	 */
+	ipv4Ranges: ipv4[];
 	/**
 	 * Restrict session creation to only the provided IPv4 ranges (		/**
 	 * Defines the behaviour of the system when a user creates multiple sessions.
 	 */
-	multiUser: SessionMultiUser = SessionMultiUser.allow;
+	multiUser: SessionMultiUser;
 	/**
 	 * Defines whether a session should be automatically killed when the connection breaks.
 	 */
-	idleAllowed: boolean = false;
+	idleAllowed: boolean;
 	/**
 	 * The lifetime duration of a session in minutes.
 	 */
-	expireTimeout: ushort = NaN;
+	expireTimeout: ushort;
 	/**
 	 * The maximum number of sessions allowed per user.
 	 */
-	maxSessions: byte = NaN;
+	maxSessions: byte;
 	
 	constructor(json: any = null) {
+		this.applications = (json["applications"] || []).slice();
+		this.ipv4Ranges = (json["ipv4Ranges"] || []).slice();
+		this.multiUser = SessionMultiUser[json["multiUser"] as SessionMultiUser] || SessionMultiUser.allow;
+		this.idleAllowed = !!json["idleAllowed"];
+		this.expireTimeout = json["expireTimeout"];
+		this.maxSessions = json["maxSessions"];
 	}
 }
