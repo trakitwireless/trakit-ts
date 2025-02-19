7297e7df94f43009fff7189cd014b9e87478b7b2
Alex Lein <alein@trakitwireless.com>
2025-02-19 16:08:14 -0500

    Updated TS nonsense and used some random stackoverflow code to make the toJSON outputs merge together properly

--- objects/API/Objects.ts
+++ objects/API/Objects.ts
@@ -1,13 +1,36 @@
 ﻿import { KEYS } from './Constants';
 
 /**
+ * https://stackoverflow.com/questions/49682569/typescript-merge-object-types#answer-49683575
+ * This is how you get TypeScript to properly infer the keys and values from the objects given to the {@link MERGE} function.
+ */
+type OptionalPropertyNames<T> = { [K in keyof T]-?: ({} extends { [P in K]: T[K] } ? K : never) }[keyof T];
+type SpreadProperties<L, R, K extends keyof L & keyof R> = { [P in K]: L[P] | Exclude<R[P], undefined> };
+type Id<T> = T extends infer U ? { [K in keyof U]: U[K] } : never;
+// type SpreadTwo<L, R> = Id<
+//   & Pick<L, Exclude<keyof L, keyof R>>
+//   & Pick<R, Exclude<keyof R, OptionalPropertyNames<R>>>
+//   & Pick<R, Exclude<OptionalPropertyNames<R>, keyof L>>
+//   & SpreadProperties<L, R, OptionalPropertyNames<R> & keyof L>
+// >;
+type SpreadTwo<R, L> = Id<  // L/R reversed because we don't overwrite properties
+  & Pick<L, Exclude<keyof L, keyof R>>
+  & Pick<R, Exclude<keyof R, OptionalPropertyNames<R>>>
+  & Pick<R, Exclude<OptionalPropertyNames<R>, keyof L>>
+  & SpreadProperties<L, R, OptionalPropertyNames<R> & keyof L>
+>;
+type Spread<A extends readonly [...any]> = A extends [infer L, ...infer R]
+  ? SpreadTwo<L, Spread<R>>
+  : unknown;
+
+/**
  * Does a deep copy of values from one object to another.
  * If multiple objects are given, properties are not overwritten.
  * @param target
  * @param sources
  * @returns target
  */
-export function MERGE(...sources: object[]): object {
+export function MERGE<A extends object[]>(...sources: [...A]): Spread<A> {
   const target: any = {};
   for (let i = 0; i < sources.length; i++) {
     const source: any = sources[i],
@@ -21,6 +44,7 @@ export function MERGE(...sources: object[]): object {
   }
   return target;
 }
+
 /**
  * Used internally by {@link MERGE}.
  * @param value
