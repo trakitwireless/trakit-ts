3018112f206bc3af4b938463598fa5b45b11baef
Alex Lein <alein@trakitwireless.com>
2025-08-27 18:21:39 -0400

    rollup config moved and updated to output what I need

---
+++ rollup.config.js
@@ -0,0 +1,37 @@
+import typescript from 'rollup-plugin-typescript2';
+import terser from '@rollup/plugin-terser';
+
+const obfuscate = {
+  ecma: 2020,
+  // compress: {
+  //   keep_classnames: true,
+  //   drop_console: true,
+  //   drop_debugger: true,
+  //   hoist_funs: true,
+  //   join_vars: true,
+  //   module: true,
+  //   toplevel: true,
+  // },
+  // mangle: {
+  //   keep_classnames: true,
+  // },
+};
+
+export default [
+  {
+    input: 'objects/objects.ts',
+    output: [
+      // {
+      //   file: '_publish/trakit-ts-objects.js',
+      //   format: 'es',
+      // },
+      {
+        file: '_publish/trakit-ts-objects.min.js',
+        format: 'es',
+        sourcemap: true,
+        plugins: [terser(obfuscate)]
+      }
+    ],
+    plugins: [typescript()],
+  }
+];
