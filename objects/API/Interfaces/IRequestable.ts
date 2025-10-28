65e7a9f0a30e9579d64d8cc50b0a6e726819fb69
Alex Lein <alein@trakitwireless.com>
2025-10-28 15:53:52 -0400

    This needs to be multiple returnable types for the sync system

--- objects/API/Interfaces/IRequestable.ts
+++ objects/API/Interfaces/IRequestable.ts
@@ -1,3 +1,16 @@
+import { Machine } from "../../Accounts/Machine";
+import { User } from "../../Accounts/User";
+import { UserAdvanced } from "../../Accounts/UserAdvanced";
+import { UserGeneral } from "../../Accounts/UserGeneral";
+import { IIdUlong } from "../../API/Interfaces/IIdUlong";
+import { Timezone } from "../../API/Timezone";
+import { Dashcam } from "../../Images/Dashcam";
+import { ProviderRegistration } from "../../Providers/Config/ProviderRegistration";
+import { Provider } from "../../Providers/Provider";
+import { ProviderAdvanced } from "../../Providers/ProviderAdvanced";
+import { ProviderControl } from "../../Providers/ProviderControl";
+import { ProviderGeneral } from "../../Providers/ProviderGeneral";
+import { email, guid, ulong } from "../Types";
 
 /**
  * The main interface for an object in the Trak-iT system.
@@ -9,11 +22,14 @@ export interface IRequestable {
 	 * {@link IIdUlong.id}
 	 * {@link Machine.key}
 	 * {@link Provider.id}
+	 * {@link ProviderGeneral.id}
+	 * {@link ProviderAdvanced.id}
+	 * {@link ProviderControl.id}
 	 * {@link ProviderRegistration.code}
 	 * {@link User.login}
 	 * {@link UserGeneral.login}
 	 * {@link UserAdvanced.login}
 	 * {@link Timezone.code}
 	 */
-	getKey(): string;
+	getKey(): ulong | email | guid | string;
 }
