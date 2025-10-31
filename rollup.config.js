bdf06d2b7dec11636cfb8781d3a0447c740cd6d3
Alex Lein <alein@trakitwireless.com>
2025-10-31 10:02:41 -0400

    Proper terser gooder

--- rollup.config.js
+++ rollup.config.js
@@ -3,19 +3,86 @@ import terser from '@rollup/plugin-terser';
 
 const obfuscate = {
 	ecma: 2020,
-	// compress: {
-	//   keep_classnames: true,
-	//   drop_console: true,
-	//   drop_debugger: true,
-	//   hoist_funs: true,
-	//   join_vars: true,
-	//   module: true,
-	//   toplevel: true,
-	// },
-	// mangle: {
-	// 	keep_classnames: true,
-	// 	properties: /^_/
-	// },
+	compress: {
+		drop_console: true,
+		drop_debugger: true,
+		hoist_funs: true,
+		module: true,
+		toplevel: true,
+	},
+	mangle: {
+//		keep_classnames: new RegExp([
+//	// Companies
+//	"Company"
+//	, "CompanyGeneral"
+//	, "CompanyStyle"
+//	, "CompanyDirectory"
+//	, "CompanyPolicy"
+//	, "CompanyReseller"
+//	// Accounts
+//	, "Contact"
+//	, "Machine"
+//	, "Session"
+//	, "User"
+//	, "User"
+//	, "UserGeneral"
+//	, "UserAdvanced"
+//	, "UserGroup"
+//	// Assets
+//	, "Asset"
+//	, "AssetGeneral"
+//	, "AssetAdvanced"
+//	, "AssetDispatch"
+//	// Messaging
+//	, "AssetAlert"
+//	, "AssetMessage"
+//	// Behaviours
+//	, "Behaviour"
+//	, "BehaviourScript"
+//	, "BehaviourLog"
+//	// Billing
+//	, "BillableHostingLicense"
+//	, "BillableHostingRule"
+//	, "BillingProfile"
+//	, "BillingReport"
+//	// Dispatch
+//	, "DispatchJob"
+//	, "DispatchTask"
+//	// Hosting
+//	, "Document"
+//	, "FormResult"
+//	, "FormTemplate"
+//	// Images
+//	, "Dashcam"
+//	, "Icon"
+//	, "Picture"
+//	// Maintenance
+//	, "MaintenanceSchedule"
+//	, "MaintenanceJob"
+//	// Places
+//	, "Place"
+//	// Providers
+//	, "Provider"
+//	, "ProviderGeneral"
+//	, "ProviderAdvanced"
+//	, "ProviderControl"
+//	, "ProviderScript"
+//	, "ProviderConfig"
+//	, "ProviderConfigurationType"
+//	, "ProviderConfiguration"
+//	, "ProviderRegistration"
+//	// Reports
+//	, "ReportTemplate"
+//	, "ReportSchedule"
+//	, "ReportResult"
+//].join("|")),
+		properties: {
+			regex: /^[#_]/,
+		}
+	},
+	//format: {
+	//	semicolons: false,
+	//},
 };
 
 export default [
