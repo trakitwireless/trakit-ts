af561552f850b53c8447c3fe1652a2e0ec950dbf
Alex Lein <alein@trakitwireless.com>
2025-12-12 19:03:52 -0500

    Not using TSC for build, so we remove the composite and rootDir stuff

--- rollup.config.js
+++ rollup.config.js
@@ -99,6 +99,13 @@ export default [
 				plugins: [terser(obfuscate)]
 			}
 		],
-		plugins: [typescript()],
+		plugins: [typescript({
+			tsconfig: './tsconfig.json',
+			tsconfigOverride: {
+				compilerOptions: {
+					declaration: false,
+				}
+			}
+		})],
 	}
 ];
