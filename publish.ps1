4c37cb5104e32cde7840b039a9fac9c11928bb45
Alex Lein <alein@trakitwireless.com>
2026-02-24 09:59:07 -0500

    Increment patch version with every build

--- publish.ps1
+++ publish.ps1
@@ -6,13 +6,27 @@ $publishDir = Join-Path $root '_publish'
 $indexFile = Join-Path $root 'objects\index.ts'
 $packageFile = Join-Path $root 'package.json'
 
-# Update version in objects/index.ts to match package.json
+# Increment version in package.json and update index.ts
 if ((Test-Path $packageFile) -and (Test-Path $indexFile)) {
-	$pkg = Get-Content $packageFile | ConvertFrom-Json
-	$ver = $pkg.version
-	$indexContent = Get-Content $indexFile
-	$newIndexContent = $indexContent -replace 'export const version = [^;]+;', "export const version = '$ver';"
-	Set-Content $indexFile $newIndexContent
+	$packageContent = Get-Content $packageFile
+	$regex = [regex]'"version":\s*"(\d+)\.(\d+)\.(\d+)"'
+	$match = $regex.Match($packageContent)
+	if ($match.Success) {
+		$major = [int]$match.Groups[1].Value
+		$minor = [int]$match.Groups[2].Value
+		$patch = [int]$match.Groups[3].Value + 1
+		$newVer = "$major.$minor.$patch"
+		$pkgVer = '"version": "' + $newVer + '"'
+		$newPackageContent = $packageContent -replace '"version":\s*"[^"]+"', $pkgVer
+		# Write updated package.json
+		Set-Content $packageFile $newPackageContent
+		# Update index.ts with new version
+		$indexContent = Get-Content $indexFile
+		$newIndexContent = $indexContent -replace 'export const version = [^;]+;', "export const version = '$newVer';"
+		Set-Content $indexFile $newIndexContent
+	} else {
+		Write-Host "Version format in package.json is invalid. Skipping version bump."
+	}
 }
 
 # Build rollup package
