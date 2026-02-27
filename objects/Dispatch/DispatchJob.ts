22edc8ce22b11d00fbec23ae2139f122f00c25b5
Alex Lein <alein@trakitwireless.com>
2026-02-27 13:47:40 -0500

    Dispatch helpers

--- objects/Dispatch/DispatchJob.ts
+++ objects/Dispatch/DispatchJob.ts
@@ -1,6 +1,7 @@
 import { ARRAY_TO_IDS, ARRAY_TO_JSON } from "../API/Arrays";
 import { BaseComponent } from "../API/BaseComponent";
 import { DATE, ID, IS_AN, JSON_DATE, JSON_TO_MAP, MAP_TO_JSON } from "../API/Functions";
+import { LatLngBounds } from "../API/Geography/LatLngBounds";
 import { IBelongAsset } from "../API/Interfaces/IBelongAsset";
 import { IBelongCompany } from "../API/Interfaces/IBelongCompany";
 import { IIdUlong } from "../API/Interfaces/IIdUlong";
@@ -13,6 +14,22 @@ import { FormResult } from "../Hosting/FormResult";
 import { ASSETS, COMPANIES, FORM_RESULTS } from "../storage";
 import { DispatchJobPriority } from "./DispatchJobPriority";
 import { DispatchStep } from "./DispatchStep";
+import { DispatchStepStatus } from "./DispatchStepStatus";
+
+/**
+ * Iterates through the given {@link DispatchStep}s and returns the first on that matches one of the given {@link DispatchStepStatus}s.
+ * @param steps
+ * @param statuses
+ * @return
+ **/
+function DispatchJob_findStepByStatuses(steps:DispatchStep[], statuses:DispatchStepStatus[]) {
+	for (var i = 0; i < steps.length; i++) {
+		if (statuses.includes(steps[i].status)) {
+			return steps[i];
+		}
+	}
+	return null;
+}
 
 /**
  * Some work that needs to be done by performing one or more {@link DispatchStep}s.
@@ -139,4 +156,86 @@ export class DispatchJob
 	 * The {@link id} is the key.
 	 */
 	getKey() { return this.id; }
+
+	/**
+	 * Returns a {@link LatLngBounds} which encloses all the steps.
+	 * @return
+	 */
+	getBounds() {
+		return new LatLngBounds(
+			this.steps.map(s => s.latlng)
+				.filter(l => !!l)
+		);
+	}
+
+	/**
+	 * Returns the first {@link DispatchStep} in the sequence that is not completed, or returns null.
+	 * @return
+	 */
+	getNextStep() {
+		return DispatchJob_findStepByStatuses(
+			this.steps,
+			[
+				DispatchStepStatus.pending,
+				DispatchStepStatus.onRoute,
+				DispatchStepStatus.arrived,
+			]
+		);
+	}
+	/**
+	 * Returns the {@link DispatchStep} in the sequence that is currently being worked on, or returns null.
+	 * This method often returns the same result as {@link DispatchJob#getNextStep} because it will find.
+	 * @returns 
+	 */
+	getActiveStep() {
+		return DispatchJob_findStepByStatuses(
+			this.steps,
+			[
+				//DispatchStepStatus.pending,
+				DispatchStepStatus.onRoute,
+				DispatchStepStatus.arrived,
+			]
+		);
+	}
+	/**
+	 * Returns the most recent {@link DispatchStep} in the sequence that has been updated, or null.
+	 * @returns 
+	 */
+	getLastStep() {
+		var date = DATE(0),
+			step = null;
+		for (var i = 0; i < this.steps.length; i++) {
+			var candidate = this.steps[i].updated;
+			if (candidate > date) {
+				date = candidate;
+				step = this.steps[i];
+			}
+		}
+		return step;
+	}
+	/**
+	 * Will return true if all the {@link DispatchStep.status}es match the given status.
+	 * @param status 
+	 * @returns 
+	 */
+	areAllStatuses(status: DispatchStepStatus) {
+		return this.steps.every(step => step.status === status);
+	}
+	/**
+	 * Will return true if all the {@link DispatchStep}s have a valid {@link DispatchStep.eta}, and it's from "today".
+	 * @param today 
+	 * @returns 
+	 */
+	areAllStepsScheduled(today?: Date | datetime | nothing) {
+		today = DATE(today);
+		if (isNaN(today.valueOf())) {
+			const now = new Date;
+			today = new Date(
+				now.getFullYear(),
+				now.getMonth(),
+				now.getDate()
+			);
+		}
+		return this.steps.every(step => IS_AN(step.eta.valueOf()) && step.eta >= today);
+	}
 }
