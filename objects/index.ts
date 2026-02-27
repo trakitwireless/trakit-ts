0264ec13b69c359f177163103833c6a76d450803
Alex Lein <alein@trakitwireless.com>
2026-02-27 14:40:37 -0500

    One more publish with helper methods in two

--- objects/index.ts
+++ objects/index.ts
@@ -354,7 +354,7 @@ import {
 /**
  * Version number for this release.
  */
-export const version = '0.0.19';
+export const version = '0.0.20';
 
 /**
  * The names of all main object types in the Trak-iT Object Model.
