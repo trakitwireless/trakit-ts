aa52e0c20319bcf31751700d97bcb49f84d871b8
Alex Lein <alein@trakitwireless.com>
2026-04-13 13:12:06 -0400

    Rebuilt and published

--- objects/index.ts
+++ objects/index.ts
@@ -356,7 +356,7 @@ import {
 /**
  * Version number for this release.
  */
-export const version = '0.0.25';
+export const version = '0.0.26';
 
 /**
  * The names of all main object types in the Trak-iT Object Model.
